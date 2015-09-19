4b038a0246dcdee1f90e8d83210b5a2750ed7a33
alex cai <cyy0523xc@gmail.com>
2015-09-19 19:40:15 +0800

    python2

---
+++ README.md
@@ -0,0 +1,6 @@
+## Python相关环境构建
+
+- Python2
+- Python3 
+- Scrapy：爬虫环境
+
---
+++ python2/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+name=python2
+
+# 清除已有的
+#docker stop ibbd-$name 
+#docker rm ibbd-$name 
+sudo docker rmi ibbd/$name 
+
+# 重新生成
+sudo docker build -t ibbd/$name ./
+
